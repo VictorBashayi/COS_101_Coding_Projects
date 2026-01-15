fbabbbf19b4861ff4b80e11a734ef6f7530079e8
VictorBashayi <victor1bashayi5@gmail.com>
2026-01-15 02:08:42 +0100

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+# Creating a Dictionary
+mada_dictionary = {
+    'father' : 'Àdá',
+    'house':'irehi',
+    'light': 'Ira ',
+    'hello': 'Ngwao',
+    'thinking' : 'Àrìrò',
+    'earth' : 'èhè',
+    'song' : 'àhè',
+    'truth' : 'Izeyiza',
+    'broom' : 'oohi',
+    'doctor' : 'adokita',
+    'you' : 'ewu',
+    'spoon' : 'ishivi',
+    'meat' : 'uye'
+}
+
+word = input('Enter a search word: ')
+print(mada_dictionary[word])
