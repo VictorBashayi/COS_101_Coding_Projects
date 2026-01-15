5990cb3e11912677f7f48a3d6fc47a8e1b2b9cc5
VictorBashayi <victor1bashayi5@gmail.com>
2026-01-15 02:09:26 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,19 +1,30 @@
 # Creating a Dictionary
-mada_dictionary = {
-    'father' : 'Àdá',
-    'house':'irehi',
-    'light': 'Ira ',
-    'hello': 'Ngwao',
-    'thinking' : 'Àrìrò',
-    'earth' : 'èhè',
-    'song' : 'àhè',
-    'truth' : 'Izeyiza',
-    'broom' : 'oohi',
-    'doctor' : 'adokita',
-    'you' : 'ewu',
-    'spoon' : 'ishivi',
-    'meat' : 'uye'
+languages = {
+    "ebira" : {
+        'father' : 'Àdá',
+        'house':'irehi',
+        'light': 'Ira ',
+        'hello': 'Ngwao',
+        'ara' : 'sleep',
+        'earth' : 'èhè',
+        'song' : 'àhè',
+        'truth' : 'Izeyiza',
+        'broom' : 'oohi',
+        'food' : 'ebae',
+        'you' : 'ewu',
+        'spoon' : 'ishivi',
+        'meat' : 'uye',
+        'goodbye': 'adijo',
+        'thank you': 'avo',
+        'yes' : 'ee',
+        'come' : 'bẹ',
+        'go' : 'na',
+        'look' : 're',
+        'money': 'ekehi'
+    }
 }
-
 word = input('Enter a search word: ')
-print(mada_dictionary[word])
+print(languages)
+
+
+
